f0d50b0e8af94791467194aaa46893ebb4c2677d
Wang Yucai <netowls-studio@outlook.com>
2025-01-16 15:45:46 +0800

    feat: 新增安全值结构体和新的 Object 扩展方法。

--- src/ProductiveLibrary.Common/src/ObjectExtensions.cs
+++ src/ProductiveLibrary.Common/src/ObjectExtensions.cs
@@ -12,6 +12,14 @@ namespace Niacomsoft.ProductiveLibrary
   /// <summary> 为 <see cref="object" /> 类型提供了扩展方法。 </summary>
   public static class ObjectExtensions
   {
+    #region Methods
+
+    /// <summary> 获取 <paramref name="me" /> 等效的安全值。 </summary>
+    /// <param name="me"> <see cref="object" /> </param>
+    /// <returns> <see cref="SafeValue{T}" /> 类型的值。 </returns>
+    /// <seealso cref="SafeValue{T}" />
+    public static SafeValue<object> GetSafeValue(this object me) => new SafeValue<object>(me);
+
     /// <summary>
     ///   当 <paramref name="me" /> 等于 <see langword="null" /> 时，返回 <see langword="true" />；否则返回
     ///   <see langword="false" /> 。
@@ -22,5 +30,25 @@ namespace Niacomsoft.ProductiveLibrary
     ///   <see langword="false" /> 。
     /// </returns>
     public static bool IsNull(this object me) => Doctor.DebugIf(me is null, SR.GetString(Strings.Diagnostics_message_isnull), DiagnosticsSourceCategory.Common);
+
+    /// <summary>
+    ///   当 <paramref name="me" /> 不等于 <see langword="null" /> 时，返回 <see langword="true" />；否则返回
+    ///   <see langword="false" /> 。
+    /// </summary>
+    /// <param name="me"> <see cref="object" /> </param>
+    /// <returns>
+    ///   当 <paramref name="me" /> 不等于 <see langword="null" /> 时，返回 <see langword="true" />；否则返回
+    ///   <see langword="false" /> 。
+    /// </returns>
+    /// <seealso cref="IsNull(object)" />
+    public static bool NotNull(this object me) => !me.IsNull();
+
+    /// <summary> 将 <paramref name="me" /> 强制转换为 <typeparamref name="T" /> 类型的值或对象实例。 </summary>
+    /// <typeparam name="T"> 目标类型。 </typeparam>
+    /// <param name="me"> <see cref="object" /> </param>
+    /// <returns> <typeparamref name="T" /> 类型的值或对象实例。 </returns>
+    public static T To<T>(this object me) => (T)me;
+
+    #endregion Methods
   }
 }
