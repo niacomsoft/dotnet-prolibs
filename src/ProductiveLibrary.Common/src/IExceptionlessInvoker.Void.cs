a7f156ef6a0b15e61d8db925ad6f7f8db16a91ad
Wang Yucai <netowls-studio@outlook.com>
2025-01-21 17:25:50 +0800

    feat: 新增松散参数委托定义。

---
+++ src/ProductiveLibrary.Common/src/IExceptionlessInvoker.Void.cs
@@ -0,0 +1,15 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary
+{
+  #region Interfaces
+
+  public partial interface IExceptionlessInvoker
+  {
+  }
+
+  #endregion Interfaces
+}
---
+++ src/ProductiveLibrary.Common/src/RestFunction.cs
@@ -0,0 +1,17 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary
+{
+  #region Delegates
+
+  /// <summary> 包含了松散参数、 <typeparamref name="TResult" /> 类型返回值的方法委托 </summary>
+  /// <typeparam name="TResult"> 返回值类型。 </typeparam>
+  /// <param name="args"> 参数数组。 </param>
+  /// <returns> <typeparamref name="TResult" /> 类型的返回值。 </returns>
+  public delegate TResult RestFunction<TResult>(params object[] args);
+
+  #endregion Delegates
+}
