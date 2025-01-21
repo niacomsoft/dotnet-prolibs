373dc54afa04c3075aa4e69bc882f676a13b6b4d
Wang Yucai <netowls-studio@outlook.com>
2025-01-21 16:18:31 +0800

    feat: 新增包含负载数据的事件参数基类。

---
+++ src/ProductiveLibrary.Common/src/PayloadEventArgs.cs
@@ -0,0 +1,30 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using System;
+
+namespace Niacomsoft.ProductiveLibrary
+{
+  /// <summary> 提供了包含负载数据的事件参数相关的基本方法。 </summary>
+  /// <seealso cref="EventArgs" />
+  public class PayloadEventArgs : EventArgs
+  {
+    #region Constructors
+
+    /// <summary> 初始化 <see cref="PayloadEventArgs" /> 类的新实例。 </summary>
+    /// <param name="payload"> 事件负载数据。 </param>
+    public PayloadEventArgs(object payload) => Payload = payload;
+
+    #endregion Constructors
+
+    #region Properties
+
+    /// <summary> 事件负载数据。 </summary>
+    /// <value> 获取 <see cref="object" /> 类型的值或对象实例，用于表示事件负载数据。 </value>
+    public virtual object Payload { get; }
+
+    #endregion Properties
+  }
+}
---
+++ src/ProductiveLibrary.Common/src/PayloadEventArgs`1.cs
@@ -0,0 +1,32 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary
+{
+  /// <summary> 提供了包含了 <typeparamref name="T" /> 类型的负载数据事件参数相关的基本方法。 </summary>
+  /// <typeparam name="T"> 负载数据类型。 </typeparam>
+  /// <seealso cref="PayloadEventArgs" />
+  public class PayloadEventArgs<T> : PayloadEventArgs
+  {
+    #region Constructors
+
+    /// <summary> 初始化 <see cref="PayloadEventArgs{T}" /> 类的新实例。 </summary>
+    /// <param name="payload"> 事件负载数据。 </param>
+    public PayloadEventArgs(T payload) : base(payload)
+    {
+      Payload = payload;
+    }
+
+    #endregion Constructors
+
+    #region Properties
+
+    /// <summary> 事件负载数据。 </summary>
+    /// <value> 获取 <typeparamref name="T" /> 类型的值或对象实例，用于表示事件负载数据。 </value>
+    public virtual new T Payload { get; }
+
+    #endregion Properties
+  }
+}
