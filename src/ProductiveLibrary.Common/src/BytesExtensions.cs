bba2f5ffc52e3f64b45986363dcba4afd6aeb627
Wang Yucai <netowls-studio@outlook.com>
2025-02-05 16:56:48 +0800

    feat: 新增字符串、字节数组 Encoding 编码相关的扩展方法。

--- src/ProductiveLibrary.Common/src/BytesExtensions.cs
+++ src/ProductiveLibrary.Common/src/BytesExtensions.cs
@@ -3,7 +3,12 @@
  * © 2025 WANG YUCAI.                                                                                                     *
 \* ********************************************************************************************************************** */
 
+using Niacomsoft.ProductiveLibrary.Resources;
+using Niacomsoft.ProductiveLibrary.Resources.Internal;
+
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
 namespace Niacomsoft.ProductiveLibrary
 {
@@ -11,6 +16,47 @@ namespace Niacomsoft.ProductiveLibrary
   public static class BytesExtensions
   {
     #region Methods
+
+    /// <summary>
+    ///   使用指定编码程序 <paramref name="encoding" /> 获取字节数组 <paramref name="me" /> 的字符串。
+    /// </summary>
+    /// <param name="me"> 字节数组。 </param>
+    /// <param name="encoding"> 编码程序。 </param>
+    /// <param name="startIndex"> 字节数组 <paramref name="me" /> 起始索引。 </param>
+    /// <param name="length"> 字节数组 <paramref name="me" /> 长度。 </param>
+    /// <returns> 字节数组。 </returns>
+    /// <seealso cref="Encoding" />
+    /// <exception cref="ArgumentNullException">
+    ///   当 <paramref name="encoding" /> 等于 <see langword="null" /> 时，可能引发此类型的异常。
+    /// </exception>
+    /// <exception cref="OverflowException"> 当访问 <c> me.Length </c> 属性时，可能引发此类型的异常。 </exception>
+    [SuppressMessage("Style", "IDE0046:转换为条件表达式", Justification = "<挂起>")]
+    public static string GetString(this byte[] me, Encoding encoding, int? startIndex = null, int? length = null)
+    {
+      if (me.Empty())
+      {
+        return null;
+      }
+      if (encoding.IsNull())
+      {
+        throw new ArgumentNullException(nameof(encoding), SR.Format(nameof(Strings.ArgumentNullException_with_argument_name), nameof(encoding)));
+      }
+
+      return encoding.GetString(me, startIndex ?? 0, length ?? me.Length);
+    }
+
+    /// <summary>
+    ///   使用指定编码程序 <see cref="UTF8Encoding" /> 获取字节数组 <paramref name="me" /> 的字符串。
+    /// </summary>
+    /// <param name="me"> 字节数组。 </param>
+    /// <param name="startIndex"> 字节数组 <paramref name="me" /> 起始索引。 </param>
+    /// <param name="length"> 字节数组 <paramref name="me" /> 长度。 </param>
+    /// <returns> 字节数组。 </returns>
+    /// <seealso cref="Encoding" />
+    /// <seealso cref="Encoding.UTF8" />
+    /// <seealso cref="UTF8Encoding" />
+    [SuppressMessage("Design", "Ex0100:Member may throw undocumented exception", Justification = "<挂起>")]
+    public static string GetString(this byte[] me, int? startIndex = null, int? length = null) => me.GetString(Encoding.UTF8, startIndex, length);
 
     /// <summary> 将字节数组 <paramref name="me" /> 转换为 <c> BASE-64 </c> 字符串。 </summary>
     /// <param name="me"> 字节数组。 </param>
--- src/ProductiveLibrary.Common/src/StringExtensions.cs
+++ src/ProductiveLibrary.Common/src/StringExtensions.cs
@@ -3,6 +3,13 @@
  * © 2025 WANG YUCAI.                                                                                                     *
 \* ********************************************************************************************************************** */
 
+using Niacomsoft.ProductiveLibrary.Resources;
+using Niacomsoft.ProductiveLibrary.Resources.Internal;
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
 namespace Niacomsoft.ProductiveLibrary
 {
   /// <summary> 为 <see cref="string" /> 类型提供了扩展方法。 </summary>
@@ -16,6 +23,40 @@ namespace Niacomsoft.ProductiveLibrary
     /// <seealso cref="Base64StringFormatter" />
     /// <seealso cref="IBase64StringDecoder" />
     public static byte[] FromBase64String(this string me) => Base64StringFormatter.Decoder.Decode(me);
+
+    /// <summary>
+    ///   使用指定的编码器 <see cref="UTF8Encoding" /> 获取字符串 <paramref name="me" /> 的字节数组。
+    /// </summary>
+    /// <param name="me"> 字符串。 </param>
+    /// <returns> 字节数组。 </returns>
+    /// <seealso cref="Encoding.UTF8" />
+    /// <seealso cref="UTF8Encoding" />
+    public static byte[] GetBytes(this string me) => me.GetBytes(Encoding.UTF8);
+
+    /// <summary>
+    ///   使用指定的编码器 <paramref name="encoding" /> 获取字符串 <paramref name="me" /> 的字节数组。
+    /// </summary>
+    /// <param name="me"> 字符串。 </param>
+    /// <param name="encoding"> 指定的编码程序。 </param>
+    /// <returns> 字节数组。 </returns>
+    /// <exception cref="ArgumentNullException">
+    ///   当 <paramref name="encoding" /> 等于 <see langword="null" /> 时，可能引发此类型的异常。
+    /// </exception>
+    /// <seealso cref="Encoding" />
+    [SuppressMessage("Style", "IDE0046:转换为条件表达式", Justification = "<挂起>")]
+    public static byte[] GetBytes(this string me, Encoding encoding)
+    {
+      if (me.IsEmpty())
+      {
+        return null;
+      }
+      if (encoding.IsNull())
+      {
+        throw new ArgumentNullException(nameof(encoding), SR.Format(nameof(Strings.ArgumentNullException_with_argument_name), nameof(encoding)));
+      }
+
+      return encoding.GetBytes(me);
+    }
 
     /// <summary> 获取字符串 <paramref name="me" /> 等效的安全字符串。 </summary>
     /// <param name="me"> 字符串。 </param>
