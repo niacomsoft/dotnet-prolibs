f92535c52cccc4e74111b676332fc9fcd5fae2c9
Wang Yucai <netowls-studio@outlook.com>
2025-02-05 14:19:02 +0800

    feat: 新增数值类型扩展方法。

---
+++ src/ProductiveLibrary.Common/src/NumberExtensions.Double.cs
@@ -0,0 +1,116 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using System;
+using System.Collections.Generic;
+
+namespace Niacomsoft.ProductiveLibrary
+{
+  public static partial class NumberExtensions
+  {
+    #region Methods
+
+    /// <summary>
+    ///   当 <paramref name="me" /><c> = </c><paramref name="value" /> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </summary>
+    /// <param name="me"> 数值。 </param>
+    /// <param name="value"> 数值。 </param>
+    /// <returns>
+    ///   当 <paramref name="me" /><c> = </c><paramref name="value" /> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </returns>
+    public static bool Equal(this double me, double value) => me == value;
+
+    /// <summary> 获取 <paramref name="me" /> 等效的字节数组。 </summary>
+    /// <param name="me"> 数值。 </param>
+    /// <returns> 获取 <paramref name="me" /> 等效的字节数组。 </returns>
+    /// <seealso cref="BitConverter.GetBytes(double)" />
+    public static IEnumerable<byte> GetBytes(this double me) => BitConverter.GetBytes(me);
+
+    /// <summary>
+    ///   当 <paramref name="me" /><c> &gt; </c><paramref name="value" /> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </summary>
+    /// <param name="me"> 数值。 </param>
+    /// <param name="value"> 数值。 </param>
+    /// <returns>
+    ///   当 <paramref name="me" /><c> &gt; </c><paramref name="value" /> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </returns>
+    public static bool GreaterThan(this double me, double value) => me > value;
+
+    /// <summary>
+    ///   当 <paramref name="me" /><c> &gt;= </c><paramref name="value" /> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </summary>
+    /// <param name="me"> 数值。 </param>
+    /// <param name="value"> 数值。 </param>
+    /// <returns>
+    ///   当 <paramref name="me" /><c> &gt;= </c><paramref name="value" /> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </returns>
+    public static bool GreaterThanOrEqual(this double me, double value) => me >= value;
+
+    /// <summary>
+    ///   当 <paramref name="me" /><c> &lt; 0 </c> 时，返回 <see langword="true" />；否则返回
+    ///   <see langword="false" /> 。
+    /// </summary>
+    /// <param name="me"> 数值。 </param>
+    /// <returns>
+    ///   当 <paramref name="me" /><c> &lt; 0 </c> 时，返回 <see langword="true" />；否则返回
+    ///   <see langword="false" /> 。
+    /// </returns>
+    public static bool IsNegative(this double me) => me.LessThan(0);
+
+    /// <summary>
+    ///   当 <paramref name="me" /><c> &gt; 0 </c> 时，返回 <see langword="true" />；否则返回
+    ///   <see langword="false" /> 。
+    /// </summary>
+    /// <param name="me"> 数值。 </param>
+    /// <returns>
+    ///   当 <paramref name="me" /><c> &gt; 0 </c> 时，返回 <see langword="true" />；否则返回
+    ///   <see langword="false" /> 。
+    /// </returns>
+    public static bool IsPositive(this double me) => me.GreaterThan(0);
+
+    /// <summary>
+    ///   当 <paramref name="me" /><c> = 0 </c> 时，返回 <see langword="true" />；否则返回
+    ///   <see langword="false" /> 。
+    /// </summary>
+    /// <param name="me"> 数值。 </param>
+    /// <returns>
+    ///   当 <paramref name="me" /><c> = 0 </c> 时，返回 <see langword="true" />；否则返回
+    ///   <see langword="false" /> 。
+    /// </returns>
+    public static bool IsZero(this double me) => me.Equal(0);
+
+    /// <summary>
+    ///   当 <paramref name="me" /><c> &lt; </c><paramref name="value" /> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </summary>
+    /// <param name="me"> 数值。 </param>
+    /// <param name="value"> 数值。 </param>
+    /// <returns>
+    ///   当 <paramref name="me" /><c> &lt; </c><paramref name="value" /> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </returns>
+    public static bool LessThan(this double me, double value) => me < value;
+
+    /// <summary>
+    ///   当 <paramref name="me" /><c> &lt;= </c><paramref name="value" /> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </summary>
+    /// <param name="me"> 数值。 </param>
+    /// <param name="value"> 数值。 </param>
+    /// <returns>
+    ///   当 <paramref name="me" /><c> &lt;= </c><paramref name="value" /> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </returns>
+    public static bool LessThanOrEqual(this double me, double value) => me <= value;
+
+    #endregion Methods
+  }
+}
---
+++ src/ProductiveLibrary.Common/src/NumberExtensions.cs
@@ -0,0 +1,12 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary
+{
+  /// <summary> 为数值类型提供了扩展方法。 </summary>
+  public static partial class NumberExtensions
+  {
+  }
+}
