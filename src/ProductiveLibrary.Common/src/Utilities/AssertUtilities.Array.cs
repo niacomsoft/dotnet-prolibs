e13e52f3c97233ebf1433431ef6608327d8775b1
Wang Yucai <netowls-studio@outlook.com>
2025-02-06 15:22:54 +0800

    feat: 新增数组和集合断言工具方法。

---
+++ src/ProductiveLibrary.Common/src/Utilities/AssertUtilities.Array.cs
@@ -0,0 +1,43 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using System;
+
+namespace Niacomsoft.ProductiveLibrary.Utilities
+{
+  public static partial class AssertUtilities
+  {
+    #region Methods
+
+    /// <summary>
+    ///   当数组 <paramref name="array" /> 等于 <see langword="null" /> 或长度 <c>
+    ///   <paramref name="array" />.LongLength = 0 </c> 时，返回 <see langword="true" />；否则返回
+    ///   <see langword="false" /> 。
+    /// </summary>
+    /// <param name="array"> 指定数组。 </param>
+    /// <returns> <see cref="bool" /> 类型的值。 </returns>
+    /// <seealso cref="Array" />
+    public static bool IsEmptyArray(Array array)
+    {
+      return IsNull(array) || IsZero(array.LongLength);
+    }
+
+    /// <summary>
+    ///   当数组 <paramref name="array" /> 不等于 <see langword="null" /> 且长度 <c>
+    ///   <paramref name="array" />.LongLength &gt; 0 </c> 时，返回 <see langword="true" />；否则返回
+    ///   <see langword="false" /> 。
+    /// </summary>
+    /// <param name="array"> 指定数组。 </param>
+    /// <returns> <see cref="bool" /> 类型的值。 </returns>
+    /// <seealso cref="Array" />
+    /// <seealso cref="IsEmptyArray(Array)" />
+    public static bool NotEmptyArray(Array array)
+    {
+      return !IsEmptyArray(array);
+    }
+
+    #endregion Methods
+  }
+}
---
+++ src/ProductiveLibrary.Common/src/Utilities/AssertUtilities.Enumerable.cs
@@ -0,0 +1,44 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Niacomsoft.ProductiveLibrary.Utilities
+{
+  public static partial class AssertUtilities
+  {
+    #region Methods
+
+    /// <summary>
+    ///   当 <see cref="IEnumerable{T}" /> 类型的集合等于 <see langword="null" /> 或元素个数等于 <c> 0 </c> 时，返回
+    ///   <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </summary>
+    /// <typeparam name="T"> 集合中元素的类型。 </typeparam>
+    /// <param name="collection"> 实现了 <see cref="IEnumerable{T}" /> 类型接口的对象实例。 </param>
+    /// <returns> <see cref="bool" /> 类型的值。 </returns>
+    /// <seealso cref="IEnumerable{T}" />
+    public static bool IsEmptyCollection<T>(IEnumerable<T> collection)
+    {
+      return IsNull(collection) || !collection.Any();
+    }
+
+    /// <summary>
+    ///   当 <see cref="IEnumerable{T}" /> 类型的集合不等于 <see langword="null" /> 且元素个数 <c> &gt; 0 </c>
+    ///   时，返回 <see langword="true" />；否则返回 <see langword="false" /> 。
+    /// </summary>
+    /// <typeparam name="T"> 集合中元素的类型。 </typeparam>
+    /// <param name="collection"> 实现了 <see cref="IEnumerable{T}" /> 类型接口的对象实例。 </param>
+    /// <returns> <see cref="bool" /> 类型的值。 </returns>
+    /// <seealso cref="IEnumerable{T}" />
+    /// <seealso cref="IsEmptyCollection{T}(IEnumerable{T})" />
+    public static bool NotEmptyCollection<T>(IEnumerable<T> collection)
+    {
+      return !IsEmptyCollection(collection);
+    }
+
+    #endregion Methods
+  }
+}
