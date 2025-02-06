639838aecf025e28a2ac7e3ca4c26c29e9e1e53a
Wang Yucai <netowls-studio@outlook.com>
2025-02-06 17:12:32 +0800

    feat: 新增 Boolean 类型断言方法。

---
+++ src/ProductiveLibrary.Common/src/TrueValidator.cs
@@ -0,0 +1,19 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary
+{
+  #region Delegates
+
+  /// <summary> 用于校验 <paramref name="value" /> 是否与 <see langword="true" /> 等效。 </summary>
+  /// <typeparam name="T"> 等效值类型。 </typeparam>
+  /// <param name="value">
+  ///   等效 <see langword="true" /> 的 <typeparamref name="T" /> 类型的值或对象实例。
+  /// </param>
+  /// <returns> <see cref="bool" /> 类型的值。 </returns>
+  public delegate bool TrueValidator<T>(T value);
+
+  #endregion Delegates
+}
---
+++ src/ProductiveLibrary.Common/src/TrueValidatorPresets.cs
@@ -0,0 +1,35 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using Niacomsoft.ProductiveLibrary.Utilities;
+
+namespace Niacomsoft.ProductiveLibrary
+{
+  /// <summary> 提供了一组预置的真值验证方法相关的静态 <see langword="static" /> 方法。 </summary>
+  public static class TrueValidatorPresets
+  {
+    #region Fields
+
+    /// <summary> 默认的真值验证方法。 </summary>
+    public static readonly TrueValidator<bool> Default = new TrueValidator<bool>((value) => value);
+
+    /// <summary> 当值等于 <c> 1 </c> 时返回真值的验证方法。 </summary>
+    public static readonly TrueValidator<int> EqualsOne = new TrueValidator<int>((value) => AssertUtilities.Equal(value, 1));
+
+    /// <summary> 当值与字符串 <c> True </c> 近似相等时返回真值的验证方法。 </summary>
+    public static readonly TrueValidator<string> TrueOrFalse = new TrueValidator<string>((value) => AssertUtilities.Like(value, bool.TrueString));
+
+    /// <summary> 当值等于 <c> “T/t” </c> 时返回真值的验证方法。 </summary>
+    public static readonly TrueValidator<char> TrueOrFalseCharacter = new TrueValidator<char>(value => Equals(value, 'T') || Equals(value, 't'));
+
+    /// <summary> 当值与字符串 <c> Yes </c> 近似相等时返回真值的验证方法。 </summary>
+    public static readonly TrueValidator<string> YesOrNo = new TrueValidator<string>((value) => AssertUtilities.Like(value, "Yes"));
+
+    /// <summary> 当值等于 <c> “Y/y” </c> 时返回真值的验证方法。 </summary>
+    public static readonly TrueValidator<char> YesOrNoCharacter = new TrueValidator<char>(value => Equals(value, 'Y') || Equals(value, 'y'));
+
+    #endregion Fields
+  }
+}
