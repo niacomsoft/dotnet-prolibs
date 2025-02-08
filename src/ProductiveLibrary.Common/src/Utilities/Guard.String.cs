13a54f70bfc4103c145a12da2f394ac4bdf4d664
Wang Yucai <netowls-studio@outlook.com>
2025-02-08 09:38:35 +0800

    feat: 新增 Guard String 方法。

---
+++ src/ProductiveLibrary.Common/src/Utilities/Guard.String.cs
@@ -0,0 +1,44 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary.Utilities
+{
+  public static partial class Guard
+  {
+    #region Methods
+
+    /// <summary>
+    ///   当字符串 <paramref name="s" /> 等于 <see langword="null" /> 或 <see cref="string.Empty" /> 时返回
+    ///   <paramref name="default" />；否则返回 <paramref name="s" />。
+    /// </summary>
+    /// <param name="s"> 字符串。 </param>
+    /// <param name="default">
+    ///   当字符串 <paramref name="s" /> 等于 <see langword="null" /> 或 <see cref="string.Empty" />
+    ///   时返回的默认字符串。
+    /// </param>
+    /// <returns> 字符串。 </returns>
+    public static string DefaultIfEmpty(string s, string @default)
+    {
+      return AssertUtilities.IsEmpty(s) ? @default : s;
+    }
+
+    /// <summary>
+    ///   当字符串 <paramref name="s" /> 等于 <see langword="null" />、 <see cref="string.Empty" /> 或空格符时返回
+    ///   <paramref name="default" />；否则返回 <paramref name="s" />。
+    /// </summary>
+    /// <param name="s"> 字符串。 </param>
+    /// <param name="default">
+    ///   当字符串 <paramref name="s" /> 等于 <see langword="null" />、 <see cref="string.Empty" /> 或空格符
+    ///   时返回的默认字符串。
+    /// </param>
+    /// <returns> 字符串。 </returns>
+    public static string DefaultIfWhiteSpace(string s, string @default)
+    {
+      return AssertUtilities.IsWhiteSpace(s) ? @default : s;
+    }
+
+    #endregion Methods
+  }
+}
