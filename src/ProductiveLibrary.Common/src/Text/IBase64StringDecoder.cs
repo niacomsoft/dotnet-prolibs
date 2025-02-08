e6c10c7668d1760302167155bf512172d87fa8a4
Wang Yucai <netowls-studio@outlook.com>
2025-02-08 16:55:09 +0800

    feat: 新增 BASE-64 字符串解码编码程序。

---
+++ src/ProductiveLibrary.Common/src/Text/IBase64StringDecoder.cs
@@ -0,0 +1,24 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary.Text
+{
+  #region Interfaces
+
+  /// <summary> 提供了 <c> BASE-64 </c> 字符串解码的接口。 </summary>
+  public interface IBase64StringDecoder
+  {
+    #region Methods
+
+    /// <summary> 获取 <c> BASE-64 </c> 字符串 <paramref name="base64Str" /> 的字节数组。 </summary>
+    /// <param name="base64Str"> <c> BASE-64 </c> 字符串。 </param>
+    /// <returns> 字节数组。 </returns>
+    byte[] GetBytes(string base64Str);
+
+    #endregion Methods
+  }
+
+  #endregion Interfaces
+}
---
+++ src/ProductiveLibrary.Common/src/Text/IBase64StringEncoder.cs
@@ -0,0 +1,46 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using Niacomsoft.ProductiveLibrary.IO;
+
+using System;
+
+namespace Niacomsoft.ProductiveLibrary.Text
+{
+  #region Interfaces
+
+  /// <summary> 提供了 <c> BASE-64 </c> 字符串编码的接口。 </summary>
+  public interface IBase64StringEncoder
+  {
+    #region Methods
+
+    /// <summary> 格式化字节数组为 <c> BASE-64 </c> 字符串。 </summary>
+    /// <param name="input"> 字节数组。 </param>
+    /// <param name="options">
+    ///   格式化配置选项。
+    ///   <para> <see cref="Base64FormattingOptions" /> 枚举类型中的一个值。 </para>
+    /// </param>
+    /// <param name="offset"> 字节数组偏移量。 </param>
+    /// <param name="length"> 字节数组长度。 </param>
+    /// <returns> <c> BASE-64 </c> 字符串。 </returns>
+    /// <seealso cref="Base64FormattingOptions" />
+    string GetString(byte[] input, Base64FormattingOptions options = Base64FormattingOptions.None, int? offset = null, int? length = null);
+
+    /// <summary> 格式化字节数组为 <c> BASE-64 </c> 字符串。 </summary>
+    /// <param name="input"> 字节数组。 </param>
+    /// <param name="options">
+    ///   格式化配置选项。
+    ///   <para> <see cref="Base64FormattingOptions" /> 枚举类型中的一个值。 </para>
+    /// </param>
+    /// <returns> <c> BASE-64 </c> 字符串。 </returns>
+    /// <seealso cref="Base64FormattingOptions" />
+    /// <seealso cref="Bytes" />
+    string GetString(Bytes input, Base64FormattingOptions options = Base64FormattingOptions.None);
+
+    #endregion Methods
+  }
+
+  #endregion Interfaces
+}
