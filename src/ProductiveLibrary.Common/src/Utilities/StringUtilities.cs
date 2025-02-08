9797476a960de60ddc809bfb2f4dc269f05b419a
Wang Yucai <netowls-studio@outlook.com>
2025-02-08 15:45:04 +0800

    feat: 新增 GetString 方法。

--- src/ProductiveLibrary.Common/src/Utilities/StringUtilities.cs
+++ src/ProductiveLibrary.Common/src/Utilities/StringUtilities.cs
@@ -3,6 +3,7 @@
  * © 2025 WANG YUCAI.                                                                                                     *
 \* ********************************************************************************************************************** */
 
+using Niacomsoft.ProductiveLibrary.IO;
 using Niacomsoft.ProductiveLibrary.Resources;
 using Niacomsoft.ProductiveLibrary.Resources.Internal;
 using Niacomsoft.ProductiveLibrary.Text;
@@ -17,6 +18,20 @@ namespace Niacomsoft.ProductiveLibrary.Utilities
   public static class StringUtilities
   {
     #region Methods
+
+    /// <summary>
+    ///   当 <paramref name="encoding" /> 等于 <see langword="null" /> 时，将引发一个
+    ///   <see cref="ArgumentNullException" /> 类型的异常。
+    /// </summary>
+    /// <param name="encoding"> 需要校验的编码程序。 </param>
+    [SuppressMessage("Design", "Ex0100:Member may throw undocumented exception", Justification = "<挂起>")]
+    private static void NotSupportEncoding(Encoding encoding)
+    {
+      if (AssertUtilities.IsNull(encoding))
+      {
+        throw new ArgumentNullException(nameof(encoding), SR.Format(nameof(Strings.ArgumentNullException_with_argument_name), nameof(encoding)));
+      }
+    }
 
     /// <summary>
     ///   使用默认编码器 <c> <see cref="DefaultEncodingHolder.DefaultEncoding" />.Value </c> 获取字符串
@@ -61,12 +76,120 @@ namespace Niacomsoft.ProductiveLibrary.Utilities
       {
         return null;
       }
-      if (AssertUtilities.IsNull(encoding))
-      {
-        throw new ArgumentNullException(nameof(encoding), SR.Format(nameof(Strings.ArgumentNullException_with_argument_name), nameof(encoding)));
-      }
+
+      NotSupportEncoding(encoding);
 
       return encoding.GetBytes(s);
+    }
+
+    /// <summary>
+    ///   使用指定编码程序 <paramref name="encoding" /> 获取字节数组 <paramref name="input" /> 等效的字符串。
+    ///   <para>
+    ///     当 <paramref name="input" /> 等于 <see langword="null" /> 或长度等于 <c> 0 </c> 时，返回
+    ///     <see langword="null" />。
+    ///   </para>
+    /// </summary>
+    /// <param name="input"> 字节数组。 </param>
+    /// <param name="encoding">
+    ///   编码程序。
+    ///   <para> <see cref="Encoding" /> 类型的对象实例。 </para>
+    /// </param>
+    /// <param name="offset"> 字节数组 <paramref name="input" /> 的偏移量。 </param>
+    /// <param name="length"> 字节数组 <paramref name="length" /> 的长度。 </param>
+    /// <returns> 字符串。 </returns>
+    /// <seealso cref="Encoding" />
+    /// <exception cref="ArgumentNullException">
+    ///   当 <paramref name="encoding" /> 等于 <see langword="null" /> 时，将引发此类型的异常。
+    /// </exception>
+    /// <exception cref="OverflowException">
+    ///   当调用
+    ///   <see cref="Bytes.FromBytes(System.Collections.Generic.IEnumerable{byte}, int?, int?)" />
+    ///   方法时，可能引发此类型的异常。
+    /// </exception>
+    public static string GetString(byte[] input, Encoding encoding, int? offset = null, int? length = null)
+    {
+      return GetString(Bytes.FromBytes(input, offset, length), encoding);
+    }
+
+    /// <summary>
+    ///   使用指定编码程序 <paramref name="encoding" /> 获取字节数组 <paramref name="input" /> 等效的字符串。
+    ///   <para>
+    ///     当 <c> <paramref name="input" />.HasData </c> 等于 <see langword="false" /> 时，返回
+    ///     <see langword="null" />。
+    ///   </para>
+    /// </summary>
+    /// <param name="input"> 字节数组。 </param>
+    /// <param name="encoding">
+    ///   编码程序。
+    ///   <para> <see cref="Encoding" /> 类型的对象实例。 </para>
+    /// </param>
+    /// <returns> 字符串。 </returns>
+    /// <seealso cref="Bytes" />
+    /// <seealso cref="Encoding" />
+    /// <exception cref="ArgumentNullException">
+    ///   当 <paramref name="encoding" /> 等于 <see langword="null" /> 时，将引发此类型的异常。
+    /// </exception>
+    /// <exception cref="OverflowException">
+    ///   当调用
+    ///   <see cref="Bytes.FromBytes(System.Collections.Generic.IEnumerable{byte}, int?, int?)" />
+    ///   方法时，可能引发此类型的异常。
+    /// </exception>
+    public static string GetString(Bytes input, Encoding encoding)
+    {
+      if (!input.HasData)
+      {
+        return null;
+      }
+
+      NotSupportEncoding(encoding);
+
+      return encoding.GetString(input.Data, input.Offset, input.Length);
+    }
+
+    /// <summary>
+    ///   使用默认编码程序 <c> <see cref="DefaultEncodingHolder.DefaultEncoding" />.Value </c> 获取字节数组
+    ///   <paramref name="input" /> 等效的字符串。
+    ///   <para>
+    ///     当 <paramref name="input" /> 等于 <see langword="null" /> 或长度等于 <c> 0 </c> 时，返回
+    ///     <see langword="null" />。
+    ///   </para>
+    /// </summary>
+    /// <param name="input"> 字节数组。 </param>
+    /// <param name="offset"> 字节数组 <paramref name="input" /> 的偏移量。 </param>
+    /// <param name="length"> 字节数组 <paramref name="length" /> 的长度。 </param>
+    /// <returns> 字符串。 </returns>
+    /// <seealso cref="DefaultEncodingHolder" />
+    /// <seealso cref="Encoding" />
+    /// <exception cref="OverflowException">
+    ///   当调用
+    ///   <see cref="Bytes.FromBytes(System.Collections.Generic.IEnumerable{byte}, int?, int?)" />
+    ///   方法时，可能引发此类型的异常。
+    /// </exception>
+    public static string GetString(byte[] input, int? offset = null, int? length = null)
+    {
+      return GetString(input, DefaultEncodingHolder.DefaultEncoding.Value, offset, length);
+    }
+
+    /// <summary>
+    ///   使用默认编码程序 <c> <see cref="DefaultEncodingHolder.DefaultEncoding" />.Value </c> 获取字节数组
+    ///   <paramref name="input" /> 等效的字符串。
+    ///   <para>
+    ///     当 <c> <paramref name="input" />.HasData </c> 等于 <see langword="false" /> 时，返回
+    ///     <see langword="null" />。
+    ///   </para>
+    /// </summary>
+    /// <param name="input"> 字节数组。 </param>
+    /// <returns> 字符串。 </returns>
+    /// <seealso cref="Bytes" />
+    /// <seealso cref="DefaultEncodingHolder" />
+    /// <exception cref="OverflowException">
+    ///   当调用
+    ///   <see cref="Bytes.FromBytes(System.Collections.Generic.IEnumerable{byte}, int?, int?)" />
+    ///   方法时，可能引发此类型的异常。
+    /// </exception>
+    public static string GetString(Bytes input)
+    {
+      return GetString(input, DefaultEncodingHolder.DefaultEncoding.Value);
     }
 
     #endregion Methods
