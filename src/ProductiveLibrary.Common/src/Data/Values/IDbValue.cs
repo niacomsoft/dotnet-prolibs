4040aa19fe0cf83a3f17ffadbedc3ed9a7c5a6a6
Wang Yucai <netowls-studio@outlook.com>
2025-02-21 14:50:28 +0800

    feat: 新增特定整型值结构体。

---
+++ src/ProductiveLibrary.Common/src/Data/Values/IDbValue.cs
@@ -0,0 +1,24 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary.Data.Values
+{
+  #region Interfaces
+
+  /// <summary> 提供了数据库值的接口。 </summary>
+  /// <typeparam name="T"> 数据库值类型。 </typeparam>
+  public interface IDbValue<T>
+  {
+    #region Properties
+
+    /// <summary> 数据库值。 </summary>
+    /// <value> 设置或获取 <typeparamref name="T" /> 类型的值或对象实例，用于表示数据库值。 </value>
+    T Value { get; }
+
+    #endregion Properties
+  }
+
+  #endregion Interfaces
+}
