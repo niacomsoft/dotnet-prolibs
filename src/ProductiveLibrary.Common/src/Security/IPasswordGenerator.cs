1976f18d1092a7b4f097c80e1e0edf98f47b4e46
Wang Yucai <netowls-studio@outlook.com>
2025-02-25 17:34:16 +0800

    feat: 新增随机密码生成接口和基类。

---
+++ src/ProductiveLibrary.Common/src/Security/IPasswordGenerator.cs
@@ -0,0 +1,44 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Niacomsoft.ProductiveLibrary.Security
+{
+  #region Interfaces
+
+  /// <summary> 提供了动态生成随机密码的接口。 </summary>
+  public interface IPasswordGenerator
+  {
+    #region Properties
+
+    /// <summary> 初始化盐值。 </summary>
+    /// <value> 获取 <see cref="SecretSalt" /> 类型的值，用于表示初始化盐值。 </value>
+    /// <seealso cref="SecretSalt" />
+    SecretSalt InitialSalt { get; }
+
+    #endregion Properties
+
+    #region Methods
+
+    /// <summary> 生成下一个随机密码。 </summary>
+    /// <param name="length"> 随机密码长度。 </param>
+    /// <returns> 随机密码字符串。 </returns>
+    string Next(int length);
+
+    /// <summary> (可等待的方法) 生成下一个随机密码。 </summary>
+    /// <param name="length"> 随机密码长度。 </param>
+    /// <param name="cancellationToken"> <see cref="CancellationToken" /> 类型的值。 </param>
+    /// <returns> 随机密码字符串。 </returns>
+    /// <seealso cref="CancellationToken" />
+    /// <seealso cref="Task{TResult}" />
+    Task<string> NextAsync(int length, CancellationToken? cancellationToken = null);
+
+    #endregion Methods
+  }
+
+  #endregion Interfaces
+}
---
+++ src/ProductiveLibrary.Common/src/Security/PasswordGenerator.cs
@@ -0,0 +1,65 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Niacomsoft.ProductiveLibrary.Security
+{
+  /// <summary> 提供了生成随机密码相关的基本方法。 </summary>
+  /// <seealso cref="IPasswordGenerator" />
+  public class PasswordGenerator : IPasswordGenerator
+  {
+    #region Constants
+
+    /// <summary> 随机密码最小长度。 </summary>
+    public const int MinimumLength = 8;
+
+    #endregion Constants
+
+    #region Constructors
+
+    /// <summary> 初始化 <see cref="PasswordGenerator" /> 类的新实例。 </summary>
+    /// <seealso cref="SecretSalt.CharacterPasswordSalt" />
+    public PasswordGenerator() : this(SecretSalt.CharacterPasswordSalt)
+    {
+    }
+
+    /// <summary> 初始化 <see cref="PasswordGenerator" /> 类的新实例。 </summary>
+    /// <param name="salt">
+    ///   生成随机密码的盐值。
+    ///   <para> <see cref="SecretSalt" /> 类型的值。 </para>
+    /// </param>
+    public PasswordGenerator(SecretSalt salt)
+    {
+      InitialSalt = salt;
+    }
+
+    #endregion Constructors
+
+    #region Properties
+
+    /// <inheritdoc />
+    public virtual SecretSalt InitialSalt { get; }
+
+    #endregion Properties
+
+    #region Methods
+
+    /// <inheritdoc />
+    public virtual string Next(int length)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    /// <inheritdoc />
+    public virtual Task<string> NextAsync(int length, CancellationToken? cancellationToken = null)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    #endregion Methods
+  }
+}
