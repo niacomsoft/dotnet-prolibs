4966199796b64cb58ba8a02ce01fb4248f2cd4e8
Wang Yucai <netowls-studio@outlook.com>
2025-03-01 17:28:10 +0800

    feat: 新增 ILogWriterFactory 工厂方法接口和抽象类。

---
+++ src/ProductiveLibrary.Logging.Abstractions/src/ILogWriterFactory.cs
@@ -0,0 +1,21 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using Niacomsoft.ProductiveLibrary.Patterns;
+
+namespace Niacomsoft.ProductiveLibrary.Runtime.Diagnostics
+{
+  #region Interfaces
+
+  /// <summary> 提供了创建实现了 <see cref="ILogWriter" /> 类型接口的对象实例的工厂方法接口。 </summary>
+  /// <seealso cref="IKnownTypeFactory{TResult}" />
+  /// <seealso cref="ILogWriter" />
+  /// <seealso cref="IUnknownTypeFactory{TResult}" />
+  public interface ILogWriterFactory : IKnownTypeFactory<ILogWriter>, IUnknownTypeFactory<ILogWriter>
+  {
+  }
+
+  #endregion Interfaces
+}
---
+++ src/ProductiveLibrary.Logging.Abstractions/src/LogWriterFactory.cs
@@ -0,0 +1,26 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary.Runtime.Diagnostics
+{
+  /// <summary>
+  ///   提供了构建实现了 <see cref="ILogWriter" /> 类型接口的对象实例相关的抽象 <see langword="abstract" /> 方法。
+  /// </summary>
+  public abstract class LogWriterFactory : ILogWriterFactory
+  {
+    #region Methods
+
+    /// <summary> 创建 <see cref="ILogWriter" /> 类型的对象实例。 </summary>
+    /// <returns> <see cref="ILogWriter" /> 类型的对象实例。 </returns>
+    public abstract ILogWriter CreateInstance();
+
+    /// <summary> 创建 <see cref="ILogWriter" /> 类型的对象实例。 </summary>
+    /// <param name="args"> 未知类型的参数数组。 </param>
+    /// <returns> <see cref="ILogWriter" /> 类型的对象实例。 </returns>
+    public abstract ILogWriter CreateInstance(params object[] args);
+
+    #endregion Methods
+  }
+}
