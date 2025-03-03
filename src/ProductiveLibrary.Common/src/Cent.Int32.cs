8c026845c9fe810f11e48104bf5043dbd94aa362
Wang Yucai <netowls-studio@outlook.com>
2025-03-03 22:13:44 +0800

    feat: 新增货币 “分” 结构体。

---
+++ src/ProductiveLibrary.Common/src/Cent.Int32.cs
@@ -0,0 +1,30 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary
+{
+  public partial struct Cent
+  {
+    #region Methods
+
+    /// <summary> 从元 <paramref name="value" /> 值转换为 <see cref="Cent" /> 类型的值。 </summary>
+    /// <param name="value"> 代表元。 </param>
+    /// <returns> <see cref="Cent" /> 类型的值。 </returns>
+    public static Cent FromJiao(int value)
+    {
+      return new Cent(value, CurrencyUnit.Yuan);
+    }
+
+    /// <summary> 从角 <paramref name="value" /> 值转换为 <see cref="Cent" /> 类型的值。 </summary>
+    /// <param name="value"> 代表角。 </param>
+    /// <returns> <see cref="Cent" /> 类型的值。 </returns>
+    public static Cent FromYuan(int value)
+    {
+      return new Cent(value, CurrencyUnit.Jiao);
+    }
+
+    #endregion Methods
+  }
+}
