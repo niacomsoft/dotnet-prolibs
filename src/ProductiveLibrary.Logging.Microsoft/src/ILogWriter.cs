6eb5ade45e92b9328728ed9cb5ca143514881af5
Wang Yucai <netowls-studio@outlook.com>
2025-03-06 14:23:23 +0800

    feat: 新增基于 ILogger 实现的记录运行时诊断日志的方法。

---
+++ src/ProductiveLibrary.Logging.Microsoft/src/ILogWriter.cs
@@ -0,0 +1,18 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+namespace Niacomsoft.ProductiveLibrary.Runtime.Diagnostics
+{
+  #region Interfaces
+
+  /// <summary> 提供了记录运行时诊断日志的接口。 </summary>
+  /// <typeparam name="T"> 类型。 </typeparam>
+  /// <seealso cref="ILogWriter" />
+  public interface ILogWriter<T> : ILogWriter
+  {
+  }
+
+  #endregion Interfaces
+}
---
+++ src/ProductiveLibrary.Logging.Microsoft/src/MicrosoftLogWriter.cs
@@ -0,0 +1,46 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using Microsoft.Extensions.Logging;
+
+namespace Niacomsoft.ProductiveLibrary.Runtime.Diagnostics
+{
+  /// <summary> 提供了基于 <see cref="ILogger" /> 记录运行时诊断日志相关的方法。 </summary>
+  /// <seealso cref="MicrosoftLogWriterBase" />
+  public class MicrosoftLogWriter : MicrosoftLogWriterBase
+  {
+    #region Constructors
+
+    /// <summary> 初始化 <see cref="MicrosoftLogWriter" /> 类的新实例。 </summary>
+    /// <param name="logger">
+    ///   记录运行时诊断日志的方法。
+    ///   <para> 实现了 <see cref="ILogger" /> 类型接口的对象实例。 </para>
+    /// </param>
+    public MicrosoftLogWriter(ILogger logger) : base(logger)
+    {
+    }
+
+    #endregion Constructors
+  }
+
+  /// <summary> 提供了基于 <see cref="ILogger{TCategoryName}" /> 记录运行时诊断日志相关的 </summary>
+  /// <typeparam name="T"> 类型。 </typeparam>
+  /// <seealso cref="MicrosoftLogWriterBase{T}" />
+  public class MicrosoftLogWriter<T> : MicrosoftLogWriterBase<T>
+  {
+    #region Constructors
+
+    /// <summary> 初始化 <see cref="MicrosoftLogWriter{T}" /> 类的新实例。 </summary>
+    /// <param name="logger">
+    ///   记录运行时诊断日志的方法。
+    ///   <para> 实现了 <see cref="ILogger{TCategoryName}" /> 类型接口的对象实例。 </para>
+    /// </param>
+    public MicrosoftLogWriter(ILogger<T> logger) : base(logger)
+    {
+    }
+
+    #endregion Constructors
+  }
+}
