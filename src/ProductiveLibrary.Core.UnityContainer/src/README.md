9cf07a1bf5530b20091854a6022b6d8386f0c25f
Wang Yucai <netowls-studio@outlook.com>
2025-04-14 22:20:13 +0800

    feat: 新增基于 UnityContainer 的动态解析服务的方法。

---
+++ src/ProductiveLibrary.Core.UnityContainer/src/README.md
@@ -0,0 +1,5 @@
+# README
+
+> 基于 `UnityContainer` 动态解析服务依赖关系的核心类库。
+>
+> A core class library based on `UnityContainer` to dynamically resolve service dependencies.
---
+++ src/ProductiveLibrary.Core.UnityContainer/src/UnityServiceResolver.cs
@@ -0,0 +1,52 @@
+﻿/* ********************************************************************************************************************** *\
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                     *
+ * © 2025 WANG YUCAI.                                                                                                     *
+\* ********************************************************************************************************************** */
+
+using System;
+
+using Unity;
+
+namespace Niacomsoft.ProductiveLibrary
+{
+  /// <summary> 提供了基于 UnityContainer 的服务解析程序相关的方法。 </summary>
+  /// <seealso cref="ServiceResolver" />
+  public class UnityServiceResolver : ServiceResolver
+  {
+    #region Constructors
+
+    /// <summary> 初始化 <see cref="UnityServiceResolver" /> 类的新实例。 </summary>
+    /// <param name="container"> 服务依赖注入容器。 </param>
+    public UnityServiceResolver(IUnityContainer container)
+    {
+      Container = container;
+    }
+
+    #endregion Constructors
+
+    #region Properties
+
+    /// <summary> 服务依赖注入容器。 </summary>
+    /// <value> 获取 <see cref="IUnityContainer" /> 类型的对象实例，用于表示服务依赖注入容器。 </value>
+    /// <seealso cref="IUnityContainer" />
+    protected virtual IUnityContainer Container { get; }
+
+    #endregion Properties
+
+    #region Methods
+
+    /// <inheritdoc />
+    protected override object ResolveImpl(string serviceName, Type serviceType)
+    {
+      return Container.Resolve(serviceType, serviceName);
+    }
+
+    /// <inheritdoc />
+    protected override object ResolveImpl(Type serviceType)
+    {
+      return Container.Resolve(serviceType);
+    }
+
+    #endregion Methods
+  }
+}
